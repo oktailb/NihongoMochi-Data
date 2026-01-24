59a2711ceed39c9296a5b3df99217e29d6774fe9
LECOQ Vincent <110967411+oktailb@users.noreply.github.com>
2026-01-24 18:15:21 +0900

    sync i18n

--- package.py
+++ package.py
@@ -16,5 +16,8 @@ def main():
         os.system(f"cd {BASE_DIR}/{lang}/grammar ; zip -q -9 ../grammar.zip *.html ; md5sum ../grammar.zip | cut -d ' ' -f 1 > ../grammar.md5")
         os.system(f"cd {BASE_DIR}/{lang}/ ; zip -q -9 ./data.zip *.json ; md5sum data.zip | cut -d ' ' -f 1 > data.md5")
 
+    os.system(f"cd common ; md5sum exercices.json | cut -d ' ' -f 1 > exercices.md5")
+    os.system(f"cd common ; md5sum grammar.json | cut -d ' ' -f 1 > grammar.md5")
+    
 if __name__ == "__main__":
     main()
