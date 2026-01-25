9601eeacd5dfa1ef8693c4ead54dba6c23f2801e
LECOQ Vincent <110967411+oktailb@users.noreply.github.com>
2026-01-25 12:36:04 +0900

    rename html archive to lessons.zip

--- package.py
+++ package.py
@@ -13,7 +13,7 @@ def main():
     for lang in langs:
         lang = lang.replace('langs/', '')
         print(f"--- Packaging de '{lang}' ---")
-        os.system(f"cd {BASE_DIR}/{lang}/grammar ; zip -q -9 ../grammar.zip *.html ; md5sum ../grammar.zip | cut -d ' ' -f 1 > ../grammar.md5")
+        os.system(f"cd {BASE_DIR}/{lang}/grammar ; zip -q -9 ../lessons.zip *.html ; md5sum ../lessons.zip | cut -d ' ' -f 1 > ../lessons.md5")
         os.system(f"cd {BASE_DIR}/{lang}/ ; zip -q -9 ./data.zip *.json ; md5sum data.zip | cut -d ' ' -f 1 > data.md5")
 
     print(f"--- Packaging exercices ---")
